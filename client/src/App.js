6915072dee11476d5e5c3794ffe8e211d07f5fc7
Lê Nhật Minh <81094010+lnm451@users.noreply.github.com>
2023-01-02 18:25:34 +0700

    First add project

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Style
 import 'bootstrap/dist/css/bootstrap.css';
 
-// Page
+// Components 
 import Login from './components/login'
 import Register from './components/register';
+import Project from './components/project';
 
 function App()
 {
+    let Component
+    switch (window.location.pathname)
+    {
+        case "/":
+            Component = Login;
+            break;
+        case "/login":
+            Component = Login;
+            break;
+        case "/register":
+            Component = Register;
+            break;
+        case "project":
+            Component = Project;
+            break;
+    }
+
     return (
-        <Router>
-            <div className='App'>
-                <Routes>
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
-                </Routes>
-            </div>
-        </Router>
+        <>
+            <Project />
+            <Component />
+        </>
+        
+        // <Router>
+        //     <div className='App'>
+        //         <Routes>
+        //             {/* TODO Change to transaction */}
+        //             <Route exact path="/" element={<Login />} />
+
+        //             <Route path='/login' element={<Login />} />
+        //             <Route path='/register' element={<Register />} />
+        //             <Route path='/project' element={<Project />} />
+        //         </Routes>
+        //     </div>
+        // </Router>
     );
 }
 
---
+++ client/src/components/project.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Project extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                <p>Project page</p>
+            </div>
+        )
+    }
+}
