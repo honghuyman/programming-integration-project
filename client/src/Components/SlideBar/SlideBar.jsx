f5266a954874df6787b0fd4e696331feb2023d73
pqtran512 <117886346+pqtran512@users.noreply.github.com>
2023-01-05 00:43:10 +0700

    Add Project Page
    Modify other components

---
+++ client/src/Components/SlideBar/SlideBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+<div className="slide-bar">
+    <div className="icon icon-menu">
+        <img src="https://cdn-icons-png.flaticon.com/512/2976/2976215.png" />
+    </div>
+    <div className="icon icon-trans">
+        <img src="https://cdn-icons-png.flaticon.com/512/2211/2211093.png" />
+        <p>Transaction</p>
+    </div>
+    <div className="icon icon-report">
+        <img src="https://cdn-icons-png.flaticon.com/512/1170/1170667.png" />
+        <p>Report</p>
+    </div>
+    <div className="icon icon-project">
+        <img src="https://cdn-icons-png.flaticon.com/512/9186/9186677.png" />
+        <p>Project</p>
+    </div>
+</div>
